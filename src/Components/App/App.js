d9f97ff3f5bfbc7d0554a47ee2de409f0190256c
GoodCookies44 <goodcookies44@gmail.com>
2024-01-20 09:07:14 +0300

    Добавил возможность добавлять несколько счётчиков

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,9 +5,11 @@ import Counter from "../Counter/Counter";
 function App() {
   return (
     <div className="App">
-      <body>
-        <Counter />
-      </body>
+      <header>
+        <Counter id="counter1" initialValue={0} />
+        <Counter id="counter2" initialValue={0} />
+        {/* Добавьте сколько угодно экземпляров Counter с разными значениями */}
+      </header>
     </div>
   );
 }
--- src/Components/Counter/Counter.js
+++ src/Components/Counter/Counter.js
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from "react";
+import PropTypes from "prop-types";
 
-const Counter = () => {
+const Counter = ({id, initialValue}) => {
   // Состояние счетчика
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(initialValue);
 
   // Функция для увеличения значения счетчика и сохранения в Chrome Storage
   const increment = () => {
@@ -24,40 +25,45 @@ const Counter = () => {
 
   // Функция для сброса значения счетчика и сохранения в Chrome Storage
   const reset = () => {
-    setCount(0);
-    saveCountToStorage(0);
+    setCount(initialValue);
+    saveCountToStorage(initialValue);
   };
 
   // Функция для сохранения значения в Chrome Storage
   const saveCountToStorage = (value) => {
-    chrome.storage.sync.set({count: value}, () => {
+    chrome.storage.sync.set({[id]: value}, () => {
       if (chrome.runtime.lastError) {
         console.error("Error saving value to storage:", chrome.runtime.lastError);
       } else {
-        console.log("Value saved to storage: " + value);
+        console.log(`Value saved to storage for ${id}: ${value}`);
       }
     });
   };
 
   // Эффект, срабатывающий при монтировании компонента, для загрузки значения из Chrome Storage
   useEffect(() => {
-    chrome.storage.sync.get(["count"], (result) => {
-      if (result.count !== undefined) {
-        setCount(result.count);
-        console.log("Value loaded from storage: " + result.count);
+    chrome.storage.sync.get([id], (result) => {
+      if (result[id] !== undefined) {
+        setCount(result[id]);
+        console.log(`Value loaded from storage for ${id}: ${result[id]}`);
       }
     });
-  }, [setCount]); // Передаем setCount в массив зависимостей
+  }, [id, setCount]); // Передаем id и setCount в массив зависимостей
 
   // Визуализация компонента
   return (
     <div>
-      <p>Счётчик: {count}</p>
+      <p>{`Счётчик ${id}: ${count}`}</p>
       <button onClick={increment}>Добавить</button>
       <button onClick={decrement}>Убавить</button>
       <button onClick={reset}>Сбросить</button>
     </div>
   );
+};
+
+Counter.propTypes = {
+  id: PropTypes.string.isRequired,
+  initialValue: PropTypes.number.isRequired,
 };
 
 export default Counter;
