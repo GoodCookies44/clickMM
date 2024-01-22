93812d251c7a2ffbc1d59613c654f2462c9ff19a
GoodCookies44 <Goodcoockies44@gmail.com>
2024-01-22 17:55:57 +0300

    Тщетные попытк

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
-import {createRoot} from "react-dom/client";
-import {BrowserRouter} from "react-router-dom";
+import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
-import App from "./App";
+import {BrowserRouter} from "react-router-dom";
+import App from "./App.js";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
