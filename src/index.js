6ee3a65dffce69c31812063887a550dcab20320c
GoodCookies44 <goodcookies44@gmail.com>
2024-01-29 00:23:21 +0300

    Довёл до рабочего состояния

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
 import App from "./App";
-import {BrowserRouter} from "react-router-dom";
+import {MemoryRouter} from "react-router-dom";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <MemoryRouter>
       <App />
-    </BrowserRouter>
+    </MemoryRouter>
   </React.StrictMode>
 );
