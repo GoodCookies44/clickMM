5ab9a590da1105853ada91fde09bd266d2713646
GoodCookies44 <goodcookies44@gmail.com>
2024-02-04 23:24:38 +0300

    Сделал ПП страницу и частично МФП

--- src/pages/MFPPage/MFPPage.js
+++ src/pages/MFPPage/MFPPage.js
@@ -1,10 +1,35 @@
+// Модули
 import React from "react";
+// Компоненты
+import Counter from "../../components/Counter/Counter";
+// Стили
 import "./MFPPage.css";
 
 function MFPPage() {
   return (
     <div>
-      <h1>MFP</h1>
+      <section className="counter__section">
+        <div className="container__MFP">
+          Запросы
+          <Counter id="MFP_request" initialValue={0} />
+        </div>
+        <div className="container__MFP">
+          ШК
+          <Counter id="MFP_SHK" initialValue={0} />
+        </div>
+        <div className="container__MFP">
+          Принято
+          <Counter id="MFP_accepted" initialValue={0} />
+        </div>
+        <div className="container__MFP">
+          Отклонено
+          <Counter id="MFP_rejected" initialValue={0} />
+        </div>
+        <div className="container__MFP">
+          На доработку
+          <Counter id="MFP_revision" initialValue={0} />
+        </div>
+      </section>
     </div>
   );
 }
--- src/pages/PPPage/PPPage.js
+++ src/pages/PPPage/PPPage.js
@@ -1,10 +1,16 @@
+// Модули
 import React from "react";
+// Компоненты
+import Counter from "../../components/Counter/Counter";
+// Стили
 import "./PPPage.css";
 
 function PPPage() {
   return (
     <div>
-      <h1>PP</h1>
+      <section className="counter__section">
+        <Counter id="PP" initialValue={0} />
+      </section>
     </div>
   );
 }
