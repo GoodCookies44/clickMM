e9de50909e27004897ce06ea6033b5a0bf438d12
GoodCookies44 <goodcookies44@gmail.com>
2024-02-22 21:10:23 +0300

    Промежуточное сохранение. Делаю смену регистра при помощи горячих клавиш

--- public/background.js
+++ public/background.js
@@ -1,1 +1,44 @@
 chrome.sidePanel.setPanelBehavior({openPanelOnActionClick: true});
+
+// Создаем контекстное меню
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.contextMenus.create({
+    id: "toggleCase",
+    title: "Toggle Case",
+    contexts: ["selection"],
+  });
+
+  chrome.contextMenus.create({
+    id: "lowerCase",
+    title: "Lower Case",
+    contexts: ["selection"],
+  });
+});
+
+// Обработчик клика по пунктам контекстного меню
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  const selectedText = info.selectionText;
+  switch (info.menuItemId) {
+    case "toggleCase":
+      chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {action: "toggleCase"});
+      });
+      break;
+    case "lowerCase":
+      chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {action: "lowerCase"});
+      });
+      break;
+    default:
+      break;
+  }
+});
+
+// Обработчик сообщений от контентного скрипта
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === "toggleCase") {
+    // Ваш код для изменения регистра текста на переключенный
+  } else if (request.action === "lowerCase") {
+    // Ваш код для изменения регистра текста на строчный
+  }
+});
--- src/pages/MKTPage/MKTPage.js
+++ src/pages/MKTPage/MKTPage.js
@@ -19,48 +19,40 @@ export default function MKTPage() {
 
         <ol>
           <li>
-            Описание в карточке не соответствует товару:{" "}
-            <Counter id="MKT1" targetIds={["MKT", "MKT_Sum"]} />
+            Описание в карточке не соответствует товару: <Counter id="MKT1" targetId="MKT" />
           </li>
           <li>
-            Пересорт: <Counter id="MKT2" targetIds={["MKT", "MKT_Sum"]} />
+            Пересорт: <Counter id="MKT2" targetId="MKT" />
           </li>
           <li>
-            Запрещенный к продаже товар для самозанятых:{" "}
-            <Counter id="MKT3" targetIds={["MKT", "MKT_Sum"]} />
+            Фотография в карточке не соответствует товару: <Counter id="MKT3" targetId="MKT" />
           </li>
           <li>
-            Запрещенный к продаже товар: <Counter id="MKT4" targetIds={["MKT", "MKT_Sum"]} />
+            Подозрение на контрафакт: <Counter id="MKT4" targetId="MKT" />
           </li>
           <li>
-            Использование карточки для другого товара:{" "}
-            <Counter id="MKT5" targetIds={["MKT", "MKT_Sum"]} />
+            Запрещенный к продаже товар для самозанятых: <Counter id="MKT5" targetId="MKT" />
           </li>
           <li>
-            По запросу правообладателя товарного знака:{" "}
-            <Counter id="MKT6" targetIds={["MKT", "MKT_Sum"]} />
+            Запрещенный к продаже товар: <Counter id="MKT6" targetId="MKT" />
           </li>
           <li>
-            Фотография в карточке не соответствует товару:{" "}
-            <Counter id="MKT7" targetIds={["MKT", "MKT_Sum"]} />
+            Использование карточки для другого товара: <Counter id="MKT7" targetId="MKT" />
           </li>
           <li>
-            Изображение упаковки не соответствует фактической:{" "}
-            <Counter id="MKT8" targetIds={["MKT", "MKT_Sum"]} />
+            По запросу правообладателя товарного знака: <Counter id="MKT8" targetId="MKT" />
           </li>
           <li>
-            Использование фотографий другого продавца:{" "}
-            <Counter id="MKT9" targetIds={["MKT", "MKT_Sum"]} />
+            Изображение упаковки не соответствует фактической: <Counter id="MKT9" targetId="MKT" />
           </li>
           <li>
-            Подозрение на контрафакт: <Counter id="MKT10" targetIds={["MKT", "MKT_Sum"]} />
+            Использование фотографий другого продавца: <Counter id="MKT10" targetId="MKT" />
           </li>
           <li>
-            По запросу контролирующих органов: <Counter id="MKT11" targetIds={["MKT", "MKT_Sum"]} />
+            По запросу контролирующих органов: <Counter id="MKT11" targetId="MKT" />
           </li>
           <li>
-            По запросу контролирующих органов ФАС:{" "}
-            <Counter id="MKT12" targetIds={["MKT", "MKT_Sum"]} />
+            По запросу контролирующих органов ФАС: <Counter id="MKT12" targetId="MKT" />
           </li>
         </ol>
       </section>
