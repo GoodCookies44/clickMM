a6057d63ce76cd0831ff29d51859c7b4b184bafc
GoodCookies44 <goodcookies44@gmail.com>
2024-05-13 17:12:13 +0300

    Оптимизировал компонент с ссылками

--- src/pages/UsefulList/UsefulList.js
+++ src/pages/UsefulList/UsefulList.js
@@ -1,9 +1,22 @@
 // Модули
 import React from "react";
+import PropTypes from "prop-types";
 // Компоненты
 import ListItem from "../../components/ListItem/ListItem";
 // Стили
 import "./UsefulList.css";
+
+function LinkItem({url, title}) {
+  return (
+    <li>
+      <h3>
+        <a href={url} target="_blank" rel="nofollow noopenner noreferrer">
+          {title}
+        </a>
+      </h3>
+    </li>
+  );
+}
 
 export default function UsefulList() {
   return (
@@ -13,333 +26,184 @@ export default function UsefulList() {
         <ListItem text="Kaz334SS" />
 
         <h2>Аккаунты</h2>
-        <li>
-          <h3>
-            <a
-              href="https://admin.kazanexpress.ru/"
-              target="_blank"
-              rel="nofollow noopenner noreferrer"
-            >
-              Админка
-            </a>
-          </h3>
-        </li>
-        <li>
-          <h3>
-            <a
-              href="https://trello.com/b/OMWvPtZB/%D0%B1%D0%B8%D0%B7%D0%BD%D0%B5%D1%81-%D0%BC%D0%BE%D0%B4%D0%B5%D1%80%D0%B0%D1%86%D0%B8%D1%8F"
-              target="_blank"
-              rel="nofollow noopenner noreferrer"
-            >
-              Trelo
-            </a>
-          </h3>
-          <h4>
-            Логин: <ListItem text="marketmoderationdepartment@gmail.com" />
-          </h4>
-          <h4>
-            Пароль: <ListItem text="3aNY-Yc5yj" />
-          </h4>
-        </li>
+        <LinkItem url={"https://admin.kazanexpress.ru/"} title={"Админка"} />
 
-        <li>
-          <h3>
-            <a
-              href="https://magnit-market.kaiten.ru/space/349342"
-              target="_blank"
-              rel="nofollow noopenner noreferrer"
-            >
-              Kaiten
-            </a>
-          </h3>
-          <h4>
-            Логин: <ListItem text="moderator.om@kazanexpress.tech" />
-          </h4>
-          <h4>
-            Пароль: <ListItem text="Jd%2eovS" />
-          </h4>
-        </li>
+        <LinkItem
+          url={
+            "https://trello.com/b/OMWvPtZB/%D0%B1%D0%B8%D0%B7%D0%BD%D0%B5%D1%81-%D0%BC%D0%BE%D0%B4%D0%B5%D1%80%D0%B0%D1%86%D0%B8%D1%8F"
+          }
+          title={"Trelo"}
+        />
+        <h4>
+          Логин: <ListItem text="marketmoderationdepartment@gmail.com" />
+        </h4>
+        <h4>
+          Пароль: <ListItem text="3aNY-Yc5yj" />
+        </h4>
+
+        <LinkItem url={"https://magnit-market.kaiten.ru/space/349342/lists"} title={"Kaiten"} />
+        <h4>
+          Логин: <ListItem text="moderator.om@kazanexpress.tech" />
+        </h4>
+        <h4>
+          Пароль: <ListItem text="Jd%2eovS" />
+        </h4>
 
         <h2>Инструкции</h2>
 
-        <li>
-          <h3>
-            <a
-              href="https://wiki.yandex.ru/drpknowledgebase/moderacija/moderator/"
-              target="_blank"
-              rel="nofollow noopenner noreferrer"
-            >
-              Wiki
-            </a>
-          </h3>
-          <h4>
-            Логин: <ListItem text="moderator.om@kazanexpress.tech" />
-          </h4>
-          <h4>
-            Пароль: <ListItem text="Jd%2eovS" />
-          </h4>
-        </li>
+        <LinkItem
+          url={"https://wiki.yandex.ru/drpknowledgebase/moderacija/moderator/"}
+          title={"Wiki"}
+        />
+        <h4>
+          Логин: <ListItem text="moderator.om@kazanexpress.tech" />
+        </h4>
+        <h4>
+          Пароль: <ListItem text="Jd%2eovS" />
+        </h4>
 
-        <li>
-          <h3>
-            <a
-              href="https://business.kazanexpress.ru/manual/2.seller-requirements/#_2-2-%D0%B7%D0%B0%D0%BF%D1%80%D0%B5%D1%89%D0%B5%D0%BD%D0%BD%D1%8B%D0%B8-%D0%BA-%D0%BF%D1%80%D0%BE%D0%B4%D0%B0%D0%B6%D0%B5-%D1%82%D0%BE%D0%B2%D0%B0%D1%80"
-              target="_blank"
-              rel="nofollow noopenner noreferrer"
-            >
-              Запрещённые к продаже товары (KazanExpress Business)
-            </a>
-          </h3>
-        </li>
+        <LinkItem
+          url={
+            "https://business.kazanexpress.ru/manual/2.seller-requirements/#_2-2-%D0%B7%D0%B0%D0%BF%D1%80%D0%B5%D1%89%D0%B5%D0%BD%D0%BD%D1%8B%D0%B8-%D0%BA-%D0%BF%D1%80%D0%BE%D0%B4%D0%B0%D0%B6%D0%B5-%D1%82%D0%BE%D0%B2%D0%B0%D1%80"
+          }
+          title={"Запрещённые к продаже товары (KazanExpress Business)"}
+        />
 
-        <li>
-          <h3>
-            <a
-              href="https://docs.google.com/document/d/1qyYymTghihebFpa_JacU0_25w26hQqcS5M4FCLSt8R4/edit"
-              target="_blank"
-              rel="nofollow noopenner noreferrer"
-            >
-              Запрещённые к продаже товары
-            </a>
-          </h3>
-        </li>
+        <LinkItem
+          url={
+            "https://docs.google.com/document/d/1qyYymTghihebFpa_JacU0_25w26hQqcS5M4FCLSt8R4/edit"
+          }
+          title={"Запрещённые к продаже товары"}
+        />
 
-        <li>
-          <h3>
-            <a
-              href="https://docs.google.com/document/d/19DsYNyeAxqCHKem9nnnayeLvEWwMX2T8IQHc62b3l1Y/edit"
-              target="_blank"
-              rel="nofollow noopenner noreferrer"
-            >
-              Инструкция по работе модератора
-            </a>
-          </h3>
-        </li>
+        <LinkItem
+          url={
+            "https://docs.google.com/document/d/19DsYNyeAxqCHKem9nnnayeLvEWwMX2T8IQHc62b3l1Y/edit"
+          }
+          title={"Инструкция по работе модератора"}
+        />
 
-        <li>
-          <h3>
-            <a
-              href="https://docs.google.com/document/d/15BJUl0IZugXHFFU-hYjVe9UK4B7pHV_qFKX9SjHtID8/edit#heading=h.agkb70ksq255"
-              target="_blank"
-              rel="nofollow noopenner noreferrer"
-            >
-              Порядок работы по МФП
-            </a>
-          </h3>
-        </li>
+        <LinkItem
+          url={
+            "https://docs.google.com/document/d/15BJUl0IZugXHFFU-hYjVe9UK4B7pHV_qFKX9SjHtID8/edit#heading=h.agkb70ksq255"
+          }
+          title={"Порядок работы по МФП"}
+        />
 
-        <li>
-          <h3>
-            <a
-              href="https://business.kazanexpress.ru/manual/photo-moderation/#_1-%D0%BC%D0%B0%D0%BB%D0%BE-%D1%84%D0%BE%D1%82%D0%BE%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D0%B8"
-              target="_blank"
-              rel="nofollow noopenner noreferrer"
-            >
-              Критерии фотографий МФП
-            </a>
-          </h3>
-        </li>
+        <LinkItem
+          url={
+            "https://business.kazanexpress.ru/manual/photo-moderation/#_1-%D0%BC%D0%B0%D0%BB%D0%BE-%D1%84%D0%BE%D1%82%D0%BE%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D0%B8"
+          }
+          title={"Критерии фотографий МФП"}
+        />
 
-        <li>
-          <h3>
-            <a
-              href="https://docs.google.com/document/d/11ma-uLytSlroAyng0SDuNWz-OY0NGOuE6Zz6Z19ISu4/edit"
-              target="_blank"
-              rel="nofollow noopenner noreferrer"
-            >
-              Порядок работы по УМФ
-            </a>
-          </h3>
-        </li>
-
-        <li>
-          <h3>
-            <a
-              href="https://docs.google.com/spreadsheets/d/1_usX24awN6NWMahyTMsOgVJ__WXc8M3f5sb0py_uX_Y/edit#gid=0"
-              target="_blank"
-              rel="nofollow noopenner noreferrer"
-            >
-              Категории для УМФ
-            </a>
-          </h3>
-        </li>
-
-        <li>
-          <h3>
-            <a
-              href="https://docs.google.com/document/d/1KMH_yEn2eDWPmm5XCwzyLCCkbT764ZDYjEPO1cIRYeg/edit"
-              target="_blank"
-              rel="nofollow noopenner noreferrer"
-            >
-              Критерии фотографий УМФ
-            </a>
-          </h3>
-        </li>
+        <LinkItem
+          url={
+            "https://docs.google.com/document/d/11ma-uLytSlroAyng0SDuNWz-OY0NGOuE6Zz6Z19ISu4/edit"
+          }
+          title={"Порядок работы по УМФ"}
+        />
+        <LinkItem
+          url={
+            "https://docs.google.com/spreadsheets/d/1_usX24awN6NWMahyTMsOgVJ__WXc8M3f5sb0py_uX_Y/edit#gid=0"
+          }
+          title={"Категории для УМФ"}
+        />
+        <LinkItem
+          url={
+            "https://docs.google.com/document/d/1KMH_yEn2eDWPmm5XCwzyLCCkbT764ZDYjEPO1cIRYeg/edit"
+          }
+          title={"Критерии фотографий УМФ"}
+        />
 
         <h2>Графики</h2>
 
-        <li>
-          <h3>
-            <a
-              href="https://docs.google.com/spreadsheets/d/19uJP8qXDdpxmBf6UGFjSTewYD54Q4YtBCo129JYPfkA/edit?usp=sharing"
-              target="_blank"
-              rel="nofollow noopenner noreferrer"
-            >
-              График работы
-            </a>
-          </h3>
-        </li>
+        <LinkItem
+          url={
+            "https://docs.google.com/spreadsheets/d/19uJP8qXDdpxmBf6UGFjSTewYD54Q4YtBCo129JYPfkA/edit?usp=sharing"
+          }
+          title={"График работы"}
+        />
 
-        <li>
-          <h3>
-            <a
-              href="https://docs.google.com/spreadsheets/d/1BB_NBFLc_HbStfT2_sHvdCj9Ti-NYh5Hk89w5uNAnnI/edit?usp=sharing"
-              target="_blank"
-              rel="nofollow noopenner noreferrer"
-            >
-              План
-            </a>
-          </h3>
-        </li>
+        <LinkItem
+          url={
+            "https://docs.google.com/spreadsheets/d/1BB_NBFLc_HbStfT2_sHvdCj9Ti-NYh5Hk89w5uNAnnI/edit?usp=sharing"
+          }
+          title={"План"}
+        />
 
         <h2>Рабочие таблицы</h2>
 
-        <li>
-          <h3>
-            <a
-              href="https://docs.google.com/spreadsheets/d/1IaH1z6zcInuMo6WTle3bwZ6GfFccrarMGetF9kdS1Do/edit#gid=47083152"
-              target="_blank"
-              rel="nofollow noopenner noreferrer"
-            >
-              Таблица МФП №1
-            </a>
-          </h3>
-        </li>
+        <LinkItem
+          url={
+            "https://docs.google.com/spreadsheets/d/1IaH1z6zcInuMo6WTle3bwZ6GfFccrarMGetF9kdS1Do/edit#gid=47083152"
+          }
+          title={"Таблица МФП №1"}
+        />
 
-        <li>
-          <h3>
-            <a
-              href="https://docs.google.com/spreadsheets/d/1rt8qTbC8is9wvlWgtq69fvuhW07bpjqf3Cz8H2gY3lw/edit#gid=652114021"
-              target="_blank"
-              rel="nofollow noopenner noreferrer"
-            >
-              Таблица МФП №2
-            </a>
-          </h3>
-        </li>
+        <LinkItem
+          url={
+            "https://docs.google.com/spreadsheets/d/1rt8qTbC8is9wvlWgtq69fvuhW07bpjqf3Cz8H2gY3lw/edit#gid=652114021"
+          }
+          title={"Таблица МФП №2"}
+        />
 
-        <li>
-          <h3>
-            <a
-              href="https://docs.google.com/spreadsheets/d/1IeFHrpChv_TB4YwrbKnIG_tag8nT0fqLMwtOC9PPtp0/edit#gid=1369042334"
-              target="_blank"
-              rel="nofollow noopenner noreferrer"
-            >
-              Таблица УМФ №1
-            </a>
-          </h3>
-        </li>
+        <LinkItem
+          url={
+            "https://docs.google.com/spreadsheets/d/1IeFHrpChv_TB4YwrbKnIG_tag8nT0fqLMwtOC9PPtp0/edit#gid=1369042334"
+          }
+          title={"Таблица УМФ №1"}
+        />
 
-        <li>
-          <h3>
-            <a
-              href="https://docs.google.com/spreadsheets/d/1KRjHA9SgUtzb-UtTvsQrld5Kl_svVym3aT5ZHsciqaw/edit#gid=1142747448"
-              target="_blank"
-              rel="nofollow noopenner noreferrer"
-            >
-              Таблица УМФ №2
-            </a>
-          </h3>
-        </li>
+        <LinkItem
+          url={
+            "https://docs.google.com/spreadsheets/d/1KRjHA9SgUtzb-UtTvsQrld5Kl_svVym3aT5ZHsciqaw/edit#gid=1142747448"
+          }
+          title={"Таблица УМФ №2"}
+        />
 
-        <li>
-          <h3>
-            <a
-              href="https://docs.google.com/spreadsheets/d/1N5Pf9uDBwhGb_YnqgkZ8l4OJxNvJxAiM/edit#gid=107836335"
-              target="_blank"
-              rel="nofollow noopenner noreferrer"
-            >
-              Таблица Модерация брендов
-            </a>
-          </h3>
-        </li>
+        <LinkItem
+          url={
+            "https://docs.google.com/spreadsheets/d/1WXSpN7R0OgvDLD6EyP9jnjYxXAXpTfdOonEcLpcEbHw/edit#gid=957078154"
+          }
+          title={"Таблица ОНГ"}
+        />
 
-        <li>
-          <h3>
-            <a
-              href="https://docs.google.com/spreadsheets/d/1WXSpN7R0OgvDLD6EyP9jnjYxXAXpTfdOonEcLpcEbHw/edit#gid=957078154"
-              target="_blank"
-              rel="nofollow noopenner noreferrer"
-            >
-              Таблица ОНГ
-            </a>
-          </h3>
-        </li>
-
-        <li>
-          <h3>
-            <a
-              href="https://docs.google.com/spreadsheets/d/1yNYoSKsCGJqHQWjUFoVm9b1hYWXlVOm-ttCngzq9FAM/edit#gid=60749704"
-              target="_blank"
-              rel="nofollow noopenner noreferrer"
-            >
-              Таблица с заявками на блюр
-            </a>
-          </h3>
-        </li>
+        <LinkItem
+          url={
+            "https://docs.google.com/spreadsheets/d/1yNYoSKsCGJqHQWjUFoVm9b1hYWXlVOm-ttCngzq9FAM/edit#gid=60749704"
+          }
+          title={"Таблица с заявками на блюр"}
+        />
 
         <h2>Формы</h2>
 
-        <li>
-          <h3>
-            <a
-              href="https://docs.google.com/forms/d/e/1FAIpQLSfmzJaK1KObwJQ930Bq7ehJtclW1QfNdVYeI2T_aRSZzT4Qbg/viewform"
-              target="_blank"
-              rel="nofollow noopenner noreferrer"
-            >
-              Запрос на модерацию фото (МФП №1)
-            </a>
-          </h3>
-        </li>
+        <LinkItem
+          url={
+            "https://docs.google.com/forms/d/e/1FAIpQLSfmzJaK1KObwJQ930Bq7ehJtclW1QfNdVYeI2T_aRSZzT4Qbg/viewform"
+          }
+          title={"Запрос на модерацию фото (МФП №1)"}
+        />
 
-        <li>
-          <h3>
-            <a
-              href="https://docs.google.com/forms/d/e/1FAIpQLSe_0msnpS70TuZG0cqN5HKx4Q4yaFmcbTS3CkyXOt3fL9qLWg/viewform"
-              target="_blank"
-              rel="nofollow noopenner noreferrer"
-            >
-              Запрос на модерацию фото (МФП №2)
-            </a>
-          </h3>
-        </li>
+        <LinkItem
+          url={
+            "https://docs.google.com/forms/d/e/1FAIpQLSe_0msnpS70TuZG0cqN5HKx4Q4yaFmcbTS3CkyXOt3fL9qLWg/viewform"
+          }
+          title={"Запрос на модерацию фото (МФП №2)"}
+        />
 
         <h2>Прочее</h2>
 
-        <li>
-          <h3>
-            <a
-              href="https://www.devicespecifications.com/ru"
-              target="_blank"
-              rel="nofollow noopenner noreferrer"
-            >
-              Сайт для поиска размеров экрана телефонов
-            </a>
-          </h3>
-        </li>
-
-        <li>
-          <h3>
-            <a
-              href="https://get-color.ru/image/"
-              target="_blank"
-              rel="nofollow noopenner noreferrer"
-            >
-              Пипетка
-            </a>
-          </h3>
-        </li>
+        <LinkItem
+          url={"https://www.devicespecifications.com/ru"}
+          title={"Сайт для поиска размеров экрана телефонов"}
+        />
       </ul>
     </div>
   );
 }
+
+LinkItem.propTypes = {
+  url: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
