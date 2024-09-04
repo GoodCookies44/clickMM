5403acce11b81f12d14f92eb21bdb5086d9a5893
GoodCookies44 <goodcookies44@gmail.com>
2024-09-04 13:02:29 +0300

    Поменял стили у ресайзера для iframe

--- src/pages/SheetPage/SheetPage.js
+++ src/pages/SheetPage/SheetPage.js
@@ -71,7 +71,15 @@ export default function SheetPage() {
                 title="Google Sheet"
                 style={{height: `${iframeHeight}px`}}
               ></iframe>
-              <div className="iframe__resizer" onMouseDown={startResizing}></div>
+              <svg
+                className="iframe__resizer"
+                onMouseDown={startResizing}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 1920 1920"
+                fill="#F6F6F6"
+              >
+                <path d="M960.182.012 451 509.193l82.7 82.817 368.112-368.113v1472.217L533.7 1328.12l-82.7 82.7L960.182 1920l509.181-509.182-82.582-82.7-368.113 367.996V223.897l368.113 368.113 82.582-82.817z" />
+              </svg>
             </div>
           </>
         )}
